a2d4a7b58ae5445a0e2649af92c08dbf2818d7f3
Tim C. <timcollins202@gmail.com>
2024-07-21 22:11:37 -0400

    Can't do that outside vblank

--- neslib.s
+++ neslib.s
@@ -193,7 +193,7 @@ loop:
 ; PPU address has been set
 ;*****************************************************************
 .segment "ZEROPAGE"
-text_address:       .res 2 ;set to the address of the text to write
+text_address:       .res 1 ;set to the address of the text to write
 
 .segment "CODE"
 .proc write_text
